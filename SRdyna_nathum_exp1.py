c5e485299cf9e3cb0619eab40352dcda5c7c4e2a
Ida Mommenejad <ida.momennejad@gmail.com>
2019-03-15 18:52:04 -0400

    Add files via upload

--- SRdyna_nathum_exp1.py
+++ SRdyna_nathum_exp1.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from SR_no_action import SR_no_action
+from SR_Dyna_no_action import SR_Dyna_no_action
 #from dyna_replay import dyna_replay
 
-def SRclass_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
+def SRDyna_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
 
     ''' This function uses the reinfrocement learning agent class in 
         SR_no_action.py to learn.
@@ -12,8 +12,7 @@ def SRclass_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
         Nat Hum Beh paper & learns predictive representations with the 
         specified learning rate and scale. 
 
-        Note: This is not SR dyna, nor SR-MB. 
-        This agent only learns the SR.
+        Note: This is an SR dyna agent (), not SR-MB.         
 
         Inputs: 
         
@@ -21,7 +20,7 @@ def SRclass_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
         gamma: discount parameter, determines scale of predictive representations
         alpha: learning rate
         p_sample: prob sampling each of the two sequences 
-        verbose: TRUE or FALSE 
+        verbose: 0, 1, 2
 
         Outputs:
 
@@ -30,12 +29,12 @@ def SRclass_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
         memory: memory of episodes 
         episodies: # episodes it takes to reach convergence 
 
-        Ida Momennejad, NYC, 2019'''
+        Ida Momennejad, 2019'''
 
     if p_sample==None:
         p_sample= [.5,.5]
 
-    SR_agent = SR_no_action(gamma, alpha, p_sample, len(envstep))
+    SR_agent = SR_Dyna_no_action(gamma, alpha, p_sample, len(envstep))
     episodes = 0
     done = False
    
@@ -52,15 +51,21 @@ def SRclass_nathum_exp1(envstep, gamma, alpha, p_sample=None, verbose=0):
             
             s = s_new
         
-        if verbose==2:
-            if episodes % verbose ==0:                    
-                print(f'SR training episode #{episodes} Done.')       
+        if verbose==2:            
+            print(f'SR training episode #{episodes} Done.')       
         episodes += 1        
 
         if SR_agent.convergence:
-            if verbose==2:
+            if verbose==2: 
                 print (episodes,' training episodes/iterations done')
             break
+
+        else: 
+            ''' SR DYNA PART '''
+            SR_agent.dyna_replay()
+            if verbose==2:  
+                if episodes % 10 ==0:            
+                    print(f'SRDYNA episode #{episodes} Done.')
 
     return SR_agent.M, SR_agent.W , SR_agent.memory, episodes
 
